fe047a9c0d130be7ff9787f39c60892843edd71c
joaorabelo98 <joaorabello99@gmail.com>
2020-06-29 21:02:07 -0300

    styledcomponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+
+function App(): JSX.Element {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+        <GlobalStyle />
+      </>
     </div>
   );
 }
